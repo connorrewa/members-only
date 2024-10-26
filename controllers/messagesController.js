4d484a42c1ff2e480cd2fa20dd02b258006c8bbd
connorrewa <connorrewa13@hotmail.com>
2024-10-26 17:40:17 -0400

    feat: add date and time to messages

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -4,7 +4,7 @@ const pool = require('../utils/db');
 exports.getMessages = async (req, res) => {
     try {
         result = await pool.query(
-            'SELECT messages.id, messages.text, messages.title, users.first_name AS author_name FROM messages JOIN users ON messages.user_id = users.id'
+            'SELECT messages.id, messages.text, messages.title, messages.timestamp, users.first_name AS author_name FROM messages JOIN users ON messages.user_id = users.id'
         );
         const messages = result.rows;
         const membershipStatus = req.user ? req.user.membership_status : false;
@@ -37,4 +37,45 @@ exports.postNewMessage = async (req, res) => {
         console.error('error saving new msg:', err);
         res.status(500).send('Internal server error');
     }
+};
+
+exports.postMembership = async (req, res) => {
+    const userAnswer = req.body.riddleAnswer.trim().toLowerCase();
+    const correctAnswer = 'piano';
+
+    if (userAnswer == correctAnswer) {
+        const userId = req.user.id;
+        try {
+            await pool.query(
+                'UPDATE users SET membership_status = true WHERE id = $1',
+                [userId]
+            );
+            res.render('membership', {
+                message:
+                    'Congratulations! You are now a member! You can view authors and post times.',
+                isMember: true,
+            });
+        } catch (err) {
+            console.error('Error updating membership status:', err);
+            res.render('membership', {
+                message: 'There was an error. Please try again later.',
+                isMember: false,
+            });
+        }
+    } else {
+        res.render('membership', {
+            message: 'Incorrect answer. Please try again.',
+            hint: 'The answer is an instrument',
+            isMember: false,
+        });
+    }
+};
+
+exports.postLogout = (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
 };
--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -13,44 +13,8 @@ router.get('/new', messagesController.getNewMessage);
 
 router.post('/new', messagesController.postNewMessage);
 
-router.post('/logout', (req, res, next) => {
-    req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-        res.redirect('/');
-    });
-});
+router.post('/logout', messagesController.postLogout);
 
-router.post('/membership', async (req, res) => {
-    const userAnswer = req.body.riddleAnswer.trim().toLowerCase();
-    const correctAnswer = 'piano';
-
-    if (userAnswer == correctAnswer) {
-        const userId = req.user.id;
-        try {
-            await pool.query(
-                'UPDATE users SET membership_status = true WHERE id = $1',
-                [userId]
-            );
-            res.render('membership', {
-                message: 'Congratulations! You are now a member!',
-                isMember: true,
-            });
-        } catch (err) {
-            console.error('Error updating membership status:', err);
-            res.render('membership', {
-                message: 'There was an error. Please try again later.',
-                isMember: false,
-            });
-        }
-    } else {
-        res.render('membership', {
-            message: 'Incorrect answer. Please try again.',
-            hint: 'The answer is an instrument',
-            isMember: false,
-        });
-    }
-});
+router.post('/membership', messagesController.postMembership);
 
 module.exports = router;
