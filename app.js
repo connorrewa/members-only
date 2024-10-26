ee18f38c437669303b33698a04a46b891280d9ac
connorrewa <connorrewa13@hotmail.com>
2024-10-25 21:45:56 -0400

    feat: add login and signup backend functionality

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const app = express();
+const bcrypt = require('bcryptjs');
+
 const PORT = 3000;
 require('dotenv').config();
 
@@ -16,12 +18,97 @@ app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const { rows } = await pool.query(
+                'SELECT * FROM users WHERE email = $1',
+                [username]
+            );
+            const user = rows[0];
+
+            if (!user) {
+                return done(null, false, { message: 'Incorrect email' });
+            }
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+                return done(null, false, { message: 'Incorrect password' });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [
+            id,
+        ]);
+        const user = rows[0];
+
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.get('/', (req, res) => {
     res.render('login');
 });
 
 app.get('/signup', (req, res) => {
     res.render('signUp');
+});
+
+app.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+app.post('/signup', async (req, res, next) => {
+    try {
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            if (err) {
+                return next(err);
+            }
+            const { firstName, lastName } = req.body;
+            await pool.query(
+                'INSERT INTO users (email, password, first_name, last_name) VALUES ($1, $2, $3, $4)',
+                [req.body.username, hashedPassword, firstName, lastName]
+            );
+            res.redirect('/');
+        });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+app.post(
+    '/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/',
+    })
+);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 app.listen(PORT, () => {
